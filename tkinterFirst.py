26a728d1802b8d19532323cc7e87a58b3eaea2a0
minkeetan <MinKee.Tan@my.bosch.com>
2021-03-04 16:14:07 +0800

    Added path to show folder chosen

--- tkinterFirst.py
+++ tkinterFirst.py
@@ -4,6 +4,7 @@ from tkinter import *
 def askDirectory():
     global window
     window.directory =  filedialog.askdirectory()
+    folderLabel.config(text=window.directory)
     print (window.directory)
 
 def click_exitButton():
@@ -12,13 +13,19 @@ def click_exitButton():
 window = Tk()
 window.title("VCU Parser")
 
+resultPathLabel = Label(window, text = "Results path : ", width = 10) 
+resultPathLabel.grid(row = 0, column = 0, pady = 20, padx=10) 
+
+folderLabel = Label(window, width=80, anchor='w', bd=1, relief='sunken') 
+folderLabel.grid(row = 0, column = 1, pady = 20) 
+
 folderButton = Button(window, text="open folder", command=askDirectory)
-folderButton.place(x=500, y=350)
+folderButton.grid(row = 0, column = 2, pady = 20, padx=20, ipadx=2)
 
 exitButton = Button(window, text="exit", command=click_exitButton)
-exitButton.place(x=450, y=350)
+exitButton.grid(row = 1, column = 0, sticky = W, padx=10, ipadx=2, pady=300)
 
-window.geometry("600x400")
+window.geometry("800x400")
 
 window.mainloop()
 
