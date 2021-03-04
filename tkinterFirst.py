6680db33d904344af3b7e767abd428a758f153d9
minkeetan <tminkee@gmail.com>
2021-03-04 09:11:47 +0800

    Added folder and exit button

--- tkinterFirst.py
+++ tkinterFirst.py
@@ -6,11 +6,19 @@ def askDirectory():
     window.directory =  filedialog.askdirectory()
     print (window.directory)
 
+def click_exitButton():
+    exit()
+
 window = Tk()
-window.title("VCU Parser")
+window.wm_title("VCU Parser")
 
-B = Button(window, text="open folder", command=askDirectory)
+folderButton = Button(window, text="open folder", command=askDirectory)
+folderButton.place(x=500, y=350)
 
-B.pack()
+exitButton = Button(window, text="exit", command=click_exitButton)
+exitButton.place(x=450, y=350)
+
+window.geometry("600x400")
+
 window.mainloop()
 
